680a47e6886f1a55a6ac626e4ad39e94934062ef
WilliamWJD <william.jose.wjd@gmail.com>
2024-05-04 22:09:47 -0300

    feat: add detail product

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -7,7 +7,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <div className="by-3 flex items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
+    <div className="by-3 flex justify-center items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
       <Image
         src={category.imageUrl}
         alt={category.name}
